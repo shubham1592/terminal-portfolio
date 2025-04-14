39136ec52d16b0183baa898b365f3de6c8ccaadf
shubham1592 <pi.shubham1592@gmail.com>
2025-04-15 04:26:27 +0530

    fix: remove tempo dependencies and fix build issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,13 @@
 import { Suspense } from "react";
-import { useRoutes, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./components/home";
-import routes from "tempo-routes";
 
 function App() {
   return (
     <Suspense fallback={<p>Loading...</p>}>
-      <>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-        {import.meta.env.VITE_TEMPO === "true" && useRoutes(routes)}
-      </>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </Suspense>
   );
 }
